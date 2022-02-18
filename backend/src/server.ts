515f1123ee683404e5ae17e09c9ecce149df1cef
Josue <josue.silva@erzinger.com.br>
2022-02-17 22:20:41 -0300

    added users to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,19 @@
-// Libraries
 import express, { json } from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
-
-// Assets
 import { log } from './assets/logger';
 import { requestLogger } from './assets/expressRequestLogger';
+import users from './routes/users';
 
-// Routes
-
-
-// Constant values definition
 const PORT = 80;
 
-// Code
 const server = express();
 const http = createServer(server);
 
 server.use(cors());
 server.use(json());
 server.use(requestLogger)
+server.use('/users', users)
 
 http.listen(PORT);
 log(`Server available at port ${PORT}`, 'server_main');
