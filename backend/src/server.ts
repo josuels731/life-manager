7f2dcf52e130f2ad9dacfd74af0af8492ea07252
Josue <josue.silva@erzinger.com.br>
2022-02-22 13:07:14 -0300

    served static pages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { json } from 'express';
+import path from 'path';
 import { createServer } from 'http';
 import cors from 'cors';
 import { log } from './assets/logger';
@@ -13,9 +14,13 @@ const http = createServer(server);
 
 server.use(cors());
 server.use(json());
-server.use(requestLogger)
-server.use('/users', users)
-server.use('/tasks', tasks)
+server.use(express.static("public"));
+server.use(requestLogger);
+server.use('/users', users);
+server.use('/tasks', tasks);
+server.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname + '/..' + '/public/index.html'));
+});
 
 http.listen(PORT);
 log(`Server available at port ${PORT}`, 'server_main');
