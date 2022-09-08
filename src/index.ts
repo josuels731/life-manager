7e12808ce30ac2fe4e51d6496a61bbce5ddc3585
josuels731 <josuelaurindo731@gmail.com>
2022-09-07 23:57:07 -0300

    removed unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.static("public"));
 
 server.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, process.env['NODE_ENV'] === 'production' ? './..' : './..', '/public/index.html'));
+  res.sendFile(path.join(__dirname, './..', '/public/index.html'));
 });
 
 server.listen(port, () => {
